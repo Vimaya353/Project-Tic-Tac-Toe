08638eb8b976a189d06af41c4862d59451b73767
Vimaya <arionlester88@gmail.com>
2026-09-18 09:37:40 +0530

    Build game controller and game logic

--- script.js
+++ script.js
@@ -28,4 +28,77 @@ const Player = (name,mark) => {
     };
 };
 
+const GameController = (() => {
+
+    const player1 = Player("Player 1", "X");
+    const player2 = Player("Player 2", "O");
+
+    let currentPlayer = player1;
+
+    const playRound = (index) => {
+
+        if (Gameboard.getBoard()[index] !== "") {
+            return;
+        }
+
+        Gameboard.setMark(index, currentPlayer.mark);
+
+        if (checkWinner()) {
+            console.log(`${currentPlayer.name} wins!`);
+            return;
+        }
+
+        if (checkTie()) {
+            console.log("It's a tie!");
+            return;
+        }
+
+        switchPlayer();
+    };
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const checkWinner = () => {
+    const board = Gameboard.getBoard();
+
+        return winningCombinations.some((combination) => {
+            const [a, b, c] = combination;
+
+            return (
+                board[a] !== "" &&
+                board[a] === board[b] &&
+                board[a] === board[c]
+            );
+        });
+    };
+
+    const checkTie = () => {
+        return Gameboard.getBoard().every((cell) => cell !== "");
+    };
+
+    return {
+        playRound
+    };
+
+})();
+
+
+console.log(Gameboard.getBoard());
+
+
+
+
 
